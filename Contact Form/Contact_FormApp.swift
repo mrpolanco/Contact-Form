1c754e4001922768743e69018b9237d9eb6b0bda
Rich Polanco <rich.polanco@automattic.com>
2023-07-11 13:48:36 -0600

    Initial Commit

---
+++ Contact Form/Contact_FormApp.swift
@@ -0,0 +1,17 @@
+//
+//  Contact_FormApp.swift
+//  Contact Form
+//
+//  Created by Rich Polanco on 7/11/23.
+//
+
+import SwiftUI
+
+@main
+struct Contact_FormApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
